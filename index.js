b0fbae56a8f109188dc04bb3afed2aac21197226
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-24 06:31:02 +0100

    make port dynamic

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static("dist"));
 
 app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/dist/index.html");
 });
 
-app.listen(3000, () => {
-  console.log("Server started");
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
 });
